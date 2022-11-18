b8fd887ac6f080f2d34470360960dcff842ec5d4
alexandrecorlet <alexandre.santos@ccc.ufcg.edu.br>
2022-11-18 19:51:27 -0300

    Solve Dice Rolling

---
+++ beginner/lista1/dice_rolling.cpp
@@ -0,0 +1,23 @@
+/*
+ * AA Iniciante 2022.1 - Lista 01: Ad-Hoc 1
+ * J. Dice Rolling
+ * author: uncoded
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    scanf("%d", &t);
+    while (t--) {
+        int p;
+        scanf("%d", &p);
+        printf("%d\n", p / 2);
+    }
+    return 0;
+}
+
+// Time Complexity: O(t) | Space Complexity O(1), where
+// t is the number of test cases.
+
